9f5fcdbc220113444ae52be8559fd1d3468dcca7
eslavath narasimha naik <narasimhanaik591@gmail.com>
2025-07-17 09:59:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import Count from "./Count";
+import React, { createContext } from "react";
+import Parent from "./parent";
+import Child from "./Child";
+
+export const nameContext = createContext();
 
 const App = () => {
-  return <Count />;
+  const name = "kittu";
+  const fav = "chocolate"; // define fav here
+
+  return (
+    <nameContext.Provider value={{ name, fav }}>
+      <div>
+        <Parent />
+        <Child />
+      </div>
+    </nameContext.Provider>
+  );
 };
 
 export default App;
